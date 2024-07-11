dca8d55236a1c30c50492bc7beca2bf819b29e14
Thirafi <132630759+Thirapi@users.noreply.github.com>
2024-07-11 02:10:51 +0000

    suit not suite

--- s.js
+++ s.js
@@ -1,52 +1,104 @@
-const btn = document.getElementById('btn');
-btn.addEventListener('click', function(){
-    document.body.classList.toggle('bg-dark');
+function computer() {
+    const comp = Math.random();
+    if( comp < 0.34 ) return 'gajah';
+    if( comp >= 0.34 && comp < 0.67 ) return 'orang';
+    return 'semut';
+};
+
+function getValue(comp, player) {
+    if( player == comp )
+        return 'SERI!';
+    if( player == 'gajah' )
+        return ( comp == 'orang' ) ? 'MENANG!' : 'KALAH!';
+    if( player == 'orang' ) 
+        return ( comp == 'gajah' ) ? 'KALAH!' : 'MENANG!';
+    if( player == 'semut' )
+        return ( comp == 'orang' ) ? 'KALAH' : 'MENANG!';
+}
+
+function roll() {
+    const computerImg = document.querySelector('.img-komputer');
+    const imageGOS = ['gajah', 'orang', 'semut'];
+    let i = 0;
+    const thisTime = new Date().getTime();
+    setInterval(function() {
+        if( new Date().getTime() - thisTime > 1000 ) {
+            clearInterval;
+            return;
+        }
+        computerImg.setAttribute('src', 'img/'+ imageGOS[i++] + '.png');
+        if( i == imageGOS.length ) i = 0;
+    }, 100)
+}
+
+const pilihan = document.querySelectorAll('li img');
+pilihan.forEach(function(pil){
+    pil.addEventListener('click', function(){
+    const pilComp = computer();
+    const pilPlayer = pil.className;
+    const hasil = getValue(pilComp, pilPlayer);
+
+    roll();
+
+    setTimeout(function() {
+        const imgComp = document.querySelector('.img-komputer');
+        imgComp.setAttribute('src', 'img/' + pilComp + '.png');
     
+        const info = document.querySelector('.info');
+        info.innerHTML = hasil;
+    }, 1000);
+    });
 });
 
-const randColor = document.createElement('h1');
-const h1Value = document.createTextNode('Randomize Color');
 
-randColor.appendChild(h1Value);
-randColor.setAttribute('class', 'font');
 
-btn.after(randColor);
 
-randColor.addEventListener('click', function(){
-    const r = Math.round(Math.random() * 255 + 1);
-    const g = Math.round(Math.random() * 255 + 1);
-    const b = Math.round(Math.random() * 255 + 1);
-    document.body.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
-});
 
-const sRed = document.querySelector('input[name=sRed]');
-const sGreen = document.querySelector('input[name=sGreen]');
-const sBlue = document.querySelector('input[name=sBlue]');
-const box = document.getElementById('box');
 
-sRed.addEventListener('input', function(){
-    const r = sRed.value;
-    const g = sGreen.value;
-    const b = sBlue.value;
-    box.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
-});
 
-sGreen.addEventListener('input', function(){
-    const r = sRed.value;
-    const g = sGreen.value;
-    const b = sBlue.value;
-    box.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
-});
 
-sBlue.addEventListener('input', function(){
-    const r = sRed.value;
-    const g = sGreen.value;
-    const b = sBlue.value;
-    box.style.backgroundColor = 'rgb('+ r +','+ g +','+ b +')';
-});
 
-document.body.addEventListener('mousemove', function(event){
-    const xPos = Math.round((event.clientX / window.innerWidth) * 255);
-    const yPos = Math.round((event.clientY / window.innerHeight) * 255);
-    document.body.style.backgroundColor = 'rgb('+ xPos +',100,'+ yPos +')';
-});
+
+
+
+
+
+
+// const pGajah = document.querySelector('.gajah');
+// pGajah.addEventListener('click', function(){
+//     const pilComp = computer();
+//     const pilPlayer = pGajah.className;
+//     const hasil = getValue(pilComp, pilPlayer);
+
+//     const imgComp = document.querySelector('.img-komputer');
+//     imgComp.setAttribute('src', 'img/' + pilComp + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
+
+// const pSemut = document.querySelector('.semut');
+// pSemut.addEventListener('click', function(){
+//     const pilComp = computer();
+//     const pilPlayer = pSemut.className;
+//     const hasil = getValue(pilComp, pilPlayer);
+
+//     const imgComp = document.querySelector('.img-komputer');
+//     imgComp.setAttribute('src', 'img/' + pilComp + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
+
+// const pOrang = document.querySelector('.orang');
+// pOrang.addEventListener('click', function(){
+//     const pilComp = computer();
+//     const pilPlayer = pOrang.className;
+//     const hasil = getValue(pilComp, pilPlayer);
+
+//     const imgComp = document.querySelector('.img-komputer');
+//     imgComp.setAttribute('src', 'img/' + pilComp + '.png');
+
+//     const info = document.querySelector('.info');
+//     info.innerHTML = hasil;
+// });
